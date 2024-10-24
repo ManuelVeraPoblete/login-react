d6f35912337cd5b56615530c160142e810d4acde
ManuelVeraPoblete <manuel.vera.poblete@gmail.com>
2024-10-24 15:43:55 -0300

    menu mejorado

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartBar, faCog, faLifeRing, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import './NavigationMenu.css';
 
 const NavigationMenu = () => {
     return (
         <nav className="navigation-menu">
             <ul>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
-                <li><Link to="/help">Help</Link></li>
-                <li><Link to="/">Logout</Link></li>
+                <li>
+                    <Link to="/dashboard">
+                        <FontAwesomeIcon icon={faChartBar} className="menu-icon" />
+                        Dashboard
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/settings">
+                        <FontAwesomeIcon icon={faCog} className="menu-icon" />
+                        Settings
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/help">
+                        <FontAwesomeIcon icon={faLifeRing} className="menu-icon" />
+                        Help
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faSignOutAlt} className="menu-icon" />
+                        Logout
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
